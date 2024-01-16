8e65b519e030420463b7899f195b13783b40737c
Shtantzer <seke.lovro@gmail.com>
2024-01-16 13:09:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Detektiranje pritiska misa
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () { 
+        
+     var audio = new Audio("sounds/tom-1.mp3");
+     audio.play();
+    
+    });
+};
+
+
+
+
+
+
+
