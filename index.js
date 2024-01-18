f985126b8750604bce3b4755358a26b70d933652
Shtantzer <seke.lovro@gmail.com>
2024-01-18 12:20:49 +0100

    Creating a sound making function with a switch statement and implementing it into click and keyboard event listener functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ for (var i=0; i<numberOfDrumButtons; i++) {
         
     var buttonInnerHTML = this.innerHTML;
 
-    console.log(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
 
     
     });
@@ -18,9 +18,47 @@ for (var i=0; i<numberOfDrumButtons; i++) {
 
 document.addEventListener("keypress", function (e) {
     
-    console.log(e);
+    makeSound(e.key);
 });
 
+
+//Zajednicka funkcija za oba event listenera
+
+function makeSound(key) {
+    switch (key){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+        break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        case "l":
+            var snare = new Audio ("sounds/snare.mp3")
+            snare.play();
+        break;
+        default:
+            alert("Wrong key was pressed...")
+            break;
+    }
+}
 
 
 
